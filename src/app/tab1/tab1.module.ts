74842520bc702b1c7f9b0a017f45d7285f40201e
Harjot kaur <hkaur1@live.maryville.edu>
2020-12-08 12:02:49 -0800

    week 3 updates

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -19,6 +19,28 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
 })
 
 
+
 export class Tab1PageModule {
+   static  groceryList  = [
+    {
+      "name": "Milk",
+      "quantity" : "2" 
+    },
+    {
+      "name": "Onions",
+      "quantity" : "4" 
+    },
+    {
+      "name": "Ketchup",
+      "quantity" : "1" 
+    },
+    {
+      "name": "Rice",
+      "quantity" : "3" 
+    },
+    {
+      "name": "Cereal",
+      "quantity" : "4" 
+    }]
 }
   
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,8 +6,82 @@ import { Component } from '@angular/core';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
+ 
   constructor() {}
+    title = "Grocery List "
+    groceryList  = [
+    {
+      "name": "Milk",
+      "quantity" : "2" 
+    },
+    {
+      "name": "Onions",
+      "quantity" : "4" 
+    },
+    {
+      "name": "Ketchup",
+      "quantity" : "1" 
+    },
+    {
+      "name": "Rice",
+      "quantity" : "3" 
+    },
+    {
+      "name": "Cereal",
+      "quantity" : "4" 
+    }];
+
+    deleteItem(deletedItem){
+      console.log("Removing item", deletedItem.name)
+      var filtered = this.groceryList.filter(function(el) { return el.name != deletedItem.name; });
+      this.groceryList = filtered;
+    }
+
+    addItem(itemName, itemQuantity){
+      this.groceryList.push({name: itemName, quantity: itemQuantity})
+    }
+    
+    addItemAlert() {
+      const alert = document.createElement('ion-alert');
+      alert.cssClass = 'my-custom-class';
+      alert.header = 'New item';
+      alert.inputs = [
+        {
+          name: 'name',
+          id:'name-id',
+          placeholder: 'Item name'
+        },
+        {
+          name: 'quantity',
+          id: 'quantity-id',
+          type: 'number',
+          min: 0,
+          placeholder: 'Item quantity'
+        }
+   
+      ];
+      alert.buttons = [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel')
+          }
+        }, {
+          text: 'Ok',
+          handler: (item) => {
+            console.log('Confirm Ok')
+            this.addItem(item.name, item.quantity)
+          }
+        }
+      ];
+    
+      document.body.appendChild(alert);
+      return alert.present();
+    }
 
 }
